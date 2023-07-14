e448b3855fdcc298ae9337e61282e2f7b4533844
tranlehaiquan <tranlehaiquan@gmail.com>
2023-07-15 01:23:06 +0700

    complete guide 5

---
+++ stacks/StorageStack.ts
@@ -0,0 +1,19 @@
+import { Bucket, StackContext, Table } from "sst/constructs";
+
+export function StorageStack({ stack }: StackContext) {
+  const bucket = new Bucket(stack, "Uploads");
+
+  // Create the DynamoDB table
+  const table = new Table(stack, "Notes", {
+    fields: {
+      userId: "string",
+      noteId: "string",
+    },
+    primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
+  });
+
+  return {
+    table,
+    bucket,
+  };
+}
