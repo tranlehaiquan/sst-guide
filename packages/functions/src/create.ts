dd658fedae586d5bc9ed6ace7c69bcd3e230a1e9
tranlehaiquan <tranlehaiquan@gmail.com>
2023-07-26 22:12:26 +0700

    7 sst guide update

--- packages/functions/src/create.ts
+++ packages/functions/src/create.ts
@@ -1,18 +1,23 @@
-import { APIGatewayProxyEvent } from 'aws-lambda';
+import { APIGatewayProxyEventV2WithIAMAuthorizer } from 'aws-lambda';
 import * as uuid from "uuid";
 import handler from "@sst-guide/core/handler";
 import dynamodb from "@sst-guide/core/dynamodb";
 import { Table } from 'sst/node/table';
+import { CognitoIdentity } from './types';
 
-export const main = handler(async (event: APIGatewayProxyEvent) => {
+export const main = handler(async (event: APIGatewayProxyEventV2WithIAMAuthorizer) => {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body || "{}");
+
+  if(!event.requestContext.authorizer.iam.cognitoIdentity) {
+    throw new Error("Missing Cognito Identity");
+  }
 
   const params = {
     TableName: Table.Notes.tableName,
     Item: {
       // The attributes of the item to be created
-      userId: "123", // The id of the author
+      userId: (event.requestContext.authorizer.iam.cognitoIdentity as CognitoIdentity).identityId,
       noteId: uuid.v1(), // A unique uuid
       content: data.content, // Parsed from request body
       attachment: data.attachment, // Parsed from request body
---
+++ packages/functions/src/types.ts
@@ -0,0 +1,3 @@
+export type CognitoIdentity = {
+  identityId: string;
+}
